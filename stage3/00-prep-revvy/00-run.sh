6cb2c1db7249b72f3ca87068268d72e51f724895
Dániel Buga <daniel.buga@revolutionrobotics.org>
2019-07-31 17:21:24 +0200

    Fixes

--- stage3/00-prep-revvy/00-run.sh
+++ stage3/00-prep-revvy/00-run.sh
@@ -5,17 +5,18 @@ echo " Start installing things that are unique to revvy "
 on_chroot << EOF
 echo "  Enable raw sockets for python for BT "
 setcap 'cap_net_raw,cap_net_admin+eip' \$(readlink -f \$(which python3))
+
+echo "  Enable i2c module "
+echo "i2c-dev" >> /etc/modules
 EOF
 
 install -m 644 files/revvy.service        "${ROOTFS_DIR}/etc/systemd/system/revvy.service"
 
 echo "  Deploy python service "
-mkdir -p "${ROOTFS_DIR}/home/pi/RevvyFramework"
 
 git clone https://github.com/RevolutionRobotics/RevvyLauncher.git
-cd RevvyLauncher
-find src -type f -exec install -D "{}" "${ROOTFS_DIR}/home/pi/RevvyFramework" \;
-cd ..
+echo "  Copying launcher to ${ROOTFS_DIR}/home/pi/RevvyFramework"
+cp -r RevvyLauncher/src "${ROOTFS_DIR}/home/pi/RevvyFramework"
 echo "  Deleting launcher sources "
 rm -rf RevvyLauncher
 
@@ -25,6 +26,7 @@ cd RevvyAlphaKit
 
 echo " Creating install package "
 python3 -m tools.create_package
+echo "  Copying install files to ${ROOTFS_DIR}/home/pi/RevvyFramework/data/ble/"
 cp install/framework.data "${ROOTFS_DIR}/home/pi/RevvyFramework/data/ble/2.data"
 cp install/framework.meta "${ROOTFS_DIR}/home/pi/RevvyFramework/data/ble/2.meta"
 
@@ -33,6 +35,8 @@ echo "  Deleting framework sources "
 rm -rf RevvyAlphaKit
 
 on_chroot << EOF
+echo "  Setting permissions on data directory "
+chmod 777 -R /home/pi/RevvyFramework/data
 echo "  Install the included package "
 python3 /home/pi/RevvyFramework/launch_revvy.py --install-only
 echo "  Enable Revvy service "
