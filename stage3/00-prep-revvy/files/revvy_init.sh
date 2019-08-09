231e07e38882ec93f41dfe7d7e4b6b13e7d86637
Daniel Buga <daniel.buga@revolutionrobotics.org>
2019-08-09 12:42:59 +0200

    Fix file permissions, remove unnecessary init script

--- stage3/00-prep-revvy/files/revvy_init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Boot time init script for Revvy
-
-# Setup the audio output on the GPIO
-/usr/bin/amixer cset numid=3 1   # playback rounte: headphone jack
-/usr/bin/amixer cset numid=1 0   # playback volume: 0 = 86%
