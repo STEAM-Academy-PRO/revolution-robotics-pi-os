63cf483b946d7711ff37eca688d85b0a00ced898
david.dudas <david.dudas@outlook.com>
2021-12-31 15:53:41 +0100

    fix cd

--- stage3/00-prep-revvy/00-run.sh
+++ stage3/00-prep-revvy/00-run.sh
@@ -45,10 +45,7 @@ if [ -d "$DIR" ]; then
 else
     echo " Downloading latest framework source "
     git clone https://github.com/RevolutionRobotics/RevvyFramework.git
-    cd RevvyFramework
 fi
-
-
 
 git clone https://github.com/RevolutionRobotics/RevvyLauncher.git
 echo "  Copying launcher to ${ROOTFS_DIR}/home/pi/RevvyFramework"
@@ -56,7 +53,7 @@ cp -r RevvyLauncher/src "${ROOTFS_DIR}/home/pi/RevvyFramework"
 echo "  Deleting launcher sources "
 rm -rf RevvyLauncher
 
-
+cd RevvyFramework
 
 echo " Creating install package "
 python3 -m dev_tools.create_package
