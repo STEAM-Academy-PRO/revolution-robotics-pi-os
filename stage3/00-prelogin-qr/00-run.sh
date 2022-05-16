7f907367ae684738a1f6978a04bf1721a8eeada1
SPapina <papina.sergei@gmail.com>
2022-05-16 18:49:06 +0300

    works, issue with amixer, need to check size of partitions, ro in next step

---
+++ stage3/00-prelogin-qr/00-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+echo "  Deploy prelogin-qr service "
+install -m 644 files/prelogin-qr.service        "${ROOTFS_DIR}/etc/systemd/system/prelogin-qr.service"
+install -m 755 files/serial.sh                  "${ROOTFS_DIR}/usr/bin/serial.sh"
+on_chroot << EOF
+echo "  Enable prelogin-qr service "
+systemctl enable prelogin-qr
+EOF
--- stage4/00-initresize/01-run.sh
+++ stage4/00-initresize/01-run.sh
@@ -3,5 +3,5 @@
 on_chroot << EOF
 systemctl disable resize2fs_once
 update-rc.d resize2fs_once remove
-rm /etc/init.d/resize2fs_once
+# rm /etc/init.d/resize2fs_once
 EOF
