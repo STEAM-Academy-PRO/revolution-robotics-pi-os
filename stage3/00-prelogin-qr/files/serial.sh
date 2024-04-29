48f118362775168c286a9b95e35a383df514fbc1
Dániel Buga <bugadani@gmail.com>
2024-04-29 12:34:18 +0200

    Simplify QR prelogin code

--- stage3/00-prelogin-qr/files/serial.sh
+++ stage3/00-prelogin-qr/files/serial.sh
@@ -2,18 +2,12 @@
 
 SERIAL=`cat /proc/cpuinfo | sed -n 's/Serial[^0]*0*//p'`
 
-/usr/bin/qrencode -t ANSI -m 2 "Revvy_${SERIAL}" -o /etc/issue
-
-if [ -f "/boot/serial_number" ]; then
-    STORED=`cat /boot/serial_number`
+if [ -f "/home/pi/serial_number" ]; then
+    STORED=`cat /home/pi/serial_number`
 else
     STORED=""
 fi
 
 if [ "${SERIAL}" != "${STORED}" ]; then
-    echo "Updating serial number in /boot/serial_number"
-    mount -o remount,rw /boot
-    echo "Revvy_${SERIAL}" | tee /boot/serial_number
-    sync /boot/serial_number
-    mount -o remount,ro /boot
+    /usr/bin/qrencode -t ANSI -m 2 "Revvy_${SERIAL}" -o /etc/issue
 fi
