dc034dd4e5aeb6e6b7fa25a7652d49591b32a2b1
Dániel Buga <bugadani@gmail.com>
2024-05-09 09:22:50 +0200

    Add simple script to display qr code manually

--- stage3/00-prelogin-qr/00-run.sh
+++ stage3/00-prelogin-qr/00-run.sh
@@ -3,6 +3,7 @@
 echo "  Deploy prelogin-qr service "
 install -m 644 files/prelogin-qr.service        "${ROOTFS_DIR}/etc/systemd/system/prelogin-qr.service"
 install -m 755 files/serial.sh                  "${ROOTFS_DIR}/usr/bin/serial.sh"
+install -m 755 files/qr.sh                      "${ROOTFS_DIR}/home/pi/qr.sh"
 on_chroot << EOF
 echo "  Enable prelogin-qr service "
 systemctl enable prelogin-qr
