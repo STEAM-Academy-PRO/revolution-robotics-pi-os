8a8672a767b038621066a34444a6e5ffd5b5fde3
Dániel Buga <bugadani@gmail.com>
2024-05-21 16:16:23 +0200

    Strip prefix from serial

--- stage3/00-prelogin-qr/files/serial.sh
+++ stage3/00-prelogin-qr/files/serial.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
 SERIAL=`cat /proc/cpuinfo | sed -n 's/Serial[^0]*0*//p'`
-QR=`/usr/bin/qrencode -t ANSI -m 2 "Revvy_${SERIAL}˙"`
+QR=`/usr/bin/qrencode -t ANSI -m 2 "${SERIAL}"`
 
 echo "Serial number:\n${QR}" > /etc/issue
